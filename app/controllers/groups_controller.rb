b216ac78b96ead08ae53e5fb4d107d91ef486988
seita_matsumoto <gyabooo@gmail.com>
2019-06-16 21:55:32 +0900

    Add group create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,20 @@
 class GroupsController < ApplicationController
   def new
+    @group = Group.new
   end
 
   def create
+    @group = Group.new(name: group_params[:name])
+    group_params[:user_ids].each do |user_id|
+      next if user_id.empty?
+      @group.members.new(user_id: user_id)
+    end
+    if @group.save
+      flash[:notice] = 'グループを作成しました'
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -10,4 +22,10 @@ class GroupsController < ApplicationController
 
   def update
   end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
+  end
 end
+
