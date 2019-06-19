9333d59f161e2bd87a760df34f977990ff9edfa4
seita_matsumoto <gyabooo@gmail.com>
2019-06-20 04:21:38 +0900

    Modify like answer

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,28 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+
   def index
-    @chat_messages = Group.find(params[:group_id]).messages.includes(:user).order(created_at: :asc)
-    @chat_message = Message.new(group_id: params[:group_id])
+    @chat_messages = @group.messages.includes(:user)
+    @chat_message = Message.new
   end
 
   def create
-    if Message.create(message_params).valid?
-      redirect_to group_messages_path
+    @chat_message = @group.messages.new(message_params)
+    if @chat_message.save
+      redirect_to group_messages_path, notice: 'メッセージが送信されました'
     else
-      redirect_to group_messages_path, alert: 'メッセージを入力してください。'
+      @chat_messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
     end
   end
 
   private
   def message_params
-    parameter = params.require(:message).permit(:body, :image)
-    parameter.merge(group_id: params[:group_id], user_id: current_user.id)
+    parameter = params.require(:message).permit(:body, :image).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,18 @@ class Group < ApplicationRecord
   has_many :members
   has_many :users, through: :members
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message_body
+    if (last_message = messages.last).present?
+      last_message.body if last_message.body?
+    else
+      'まだメッセージはありません。'
+    end
+  end
+
+  def show_last_message_img
+    if (last_message = messages.last).present?
+      last_message.image.present? ? ActionController::Base.helpers.image_tag(last_message.image.url(:thumb)) : ''
+    end
+  end
 end
