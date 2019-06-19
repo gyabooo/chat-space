424f6fa3dfaa806bc0625c52363482d90d876cb7
seita_matsumoto <gyabooo@gmail.com>
2019-06-19 22:47:04 +0900

    Add validates to message

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,5 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
   mount_uploader :image, MessageImageUploader
+  validates :image, presence: true, unless: Proc.new { |a| a.body.present? }
 end
