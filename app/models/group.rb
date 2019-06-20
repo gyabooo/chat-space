6050ad7a0eabc872ea0a4154e68eccc0d23d3f78
seita_matsumoto <gyabooo@gmail.com>
2019-06-20 18:52:59 +0900

    Modify sidebar group message

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,17 +4,15 @@ class Group < ApplicationRecord
   has_many :users, through: :members
   validates :name, presence: true, uniqueness: true
 
-  def show_last_message_body
+  def show_last_message
     if (last_message = messages.last).present?
-      last_message.body if last_message.body?
+      result = []
+      result.push last_message.body if last_message.body?
+      result.push ActionController::Base.helpers.image_tag(last_message.image.url(:thumb)) if last_message.image.present?
+      return result
     else
-      'まだメッセージはありません。'
+      ['まだメッセージはありません。']
     end
   end
 
-  def show_last_message_img
-    if (last_message = messages.last).present?
-      last_message.image.present? ? ActionController::Base.helpers.image_tag(last_message.image.url(:thumb)) : ''
-    end
-  end
 end
