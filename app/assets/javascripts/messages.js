3c5c2a5b0caa8baa1d7bbe82a9c694547a29154c
seita_matsumoto <gyabooo@gmail.com>
2019-06-28 19:17:23 +0900

    Modify ajax and add turbolinks event

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,25 +1,86 @@
 $(function () {
-  $('#new_message').on('submit', function (e) {
-    e.preventDefault();
-    //console.log('OK');
-    // create form data
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    // debugger;
-    $.ajax({
-      url: url,
-      type: 'POST',
-      data: formData,
-      processData: false,
-      contentType: false,
-      dataType: 'json',
-    })
-    .done(function(data) {
-      console.log('OK')
-    })
-    .fail(function () {
-      alert('Fail!!')
-    })
 
+  // return image html
+  function image_html(data) {
+      return `<img src="${data.image.url}" alt="${data.image_alt}">`;
+  }
+
+  // return image thumb html
+  function image_thumb_html(data) {
+    return `<img src="${data.image.thumb.url}" alt="${data.image_thumb_alt}">`;
+  }
+
+  // return main contents html
+  function buildMainHTML(data) {
+    html = `<div class="chat-message-container">
+              <div class="chat-message__info">
+                <p class="chat-message__username">${data.username}</p>
+                <p class="chat-message__date">${data.date}</p>
+              </div>
+              <div class="chat-message__message">
+                <p>${data.message}</p>
+                ${data.image.url ? image_html(data) : ''}
+              </div>
+            </div>`;
+    
+    return html;
+  }
+
+  // return side contents html
+  function buildSideHTML(data) {
+    var html = `<div class="chat-group__info">`;
+    if (data.message) {
+      html = html + `${data.message} `
+    }
+    if (data.image.url) {
+      html = html + `${image_thumb_html(data)}`
+    }
+    return html + `</div>`
+  }
+
+  function getCurSideGroup(url) {
+    var chat_groups = $(".chat-group a");
+    for (var i = 0; i < chat_groups.length; i++){
+      if ($(chat_groups[i]).attr('href') === url) return $(chat_groups[i]);
+    }
+  }
+
+  // run turbolinks event at first load, reload, page switching
+  $(document).on('turbolinks:load', function () {
+    // run ajax
+    $('#new_message').on('submit', function (e) {
+      e.preventDefault();
+
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+
+      $.ajax({
+        url: url,
+        type: 'POST',
+        data: formData,
+        dataType: "json",
+        processData: false,
+        contentType: false
+      })
+        .done(function (data) {
+          // update main contents
+          $(".contents__chat-messages")
+            .append(buildMainHTML(data))
+            .animate({ scrollTop: $(".contents__chat-messages")[0].scrollHeight });
+          $("#message_body").val('');
+          $(".chat-form__btn").prop("disabled", false);
+          // update sidebar
+          var side_group = getCurSideGroup(url);
+          side_group.find(".chat-group__info").remove();
+          side_group.append(buildSideHTML(data));
+          console.log('OK');
+          // debugger;
+        })
+        .fail(function () {
+          alert('メッセージの送信に失敗しました')
+        })
+
+    })
   })
+
 })
