e54862aa095a7c776478ced12d577245eaed1b6d
seita_matsumoto <gyabooo@gmail.com>
2019-06-28 20:23:21 +0900

    Delete comments

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,5 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
   mount_uploader :image, MessageImageUploader
-  # Add my statement
-  # validates :image, presence: true, unless: Proc.new { |a| a.body.present? }
   validates :body, presence: true, unless: :image?
 end
