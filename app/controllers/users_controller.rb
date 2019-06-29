b37a3436f1e80cec031d9b99e65f84fd175c4c75
seita_matsumoto <gyabooo@gmail.com>
2019-06-29 20:51:36 +0900

    Modify current_user.id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE ?', "#{params[:search]}%").where.not(id: params[:current_user_id])
+    @users = User.where('name LIKE ?', "#{params[:search]}%").where.not(id: current_user.id)
     respond_to do |format|
       format.html { redirect_to root_path }
       format.json
