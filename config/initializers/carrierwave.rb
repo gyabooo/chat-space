f178754fce771e1564b1868ef427c67cbc8ead73
seita_matsumoto <gyabooo@gmail.com>
2019-07-07 17:51:39 +0900

    Modify use aws role for s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,9 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+    # aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+    # aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+    use_iam_profile: true,
     region: 'ap-northeast-1'
   }
 
