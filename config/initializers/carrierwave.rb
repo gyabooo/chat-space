2735aad090eeadbd3df3c49ec980199c2b272495
seita_matsumoto <gyabooo@gmail.com>
2019-07-09 18:53:35 +0900

    Update aws accesskey per environment

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,13 +5,21 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    # aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    # aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    use_iam_profile: true,
-    region: 'ap-northeast-1'
-  }
+
+  if Rails.env.development?
+    config.fog_credentials = {
+      provider: 'AWS',
+      region: 'ap-northeast-1',
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key
+    }
+  else
+    config.fog_credentials = {
+      provider: 'AWS',
+      region: 'ap-northeast-1',
+      use_iam_profile: true
+    }
+  end
 
   config.fog_directory  = 'gyabooo-chat-space'
   config.asset_host = "https://#{config.fog_directory}.s3-ap-northeast-1.amazonaws.com"
